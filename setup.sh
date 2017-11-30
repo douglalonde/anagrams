602a7cb344ec7778ebfdb86bb279ffe86667141e
douglalonde <doug.lalonde@rubikloud.com>
2017-11-29 23:11:31 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,6 @@ pip install -r requirements.txt
 export FLASK_APP=anagrams.py
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
+echo "To start the server run:"
 echo "flask run --host=0.0.0.0"
+echo "and visit http://0.0.0.0:8000"
